ace5cf813a57dd91fafd1bcc380b22b95c908d12
MunkhUchral-19 <uchraluchka900@gmail.com>
2026-07-29 12:15:22 +0800

    Replace feature component with Header component

--- tooniimashin.js
+++ tooniimashin.js
@@ -1,10 +1,11 @@
 const uildel = document.getElementById("uildel");
 const result = document.getElementById("result");
 
-const symbols = ["x", "AC", "...", "/", 7, 8, 9, "*", 4, 5, 6, "-", 1, 2, 3, "+", "...", 0, ".", "="];
+const symbols = ["DEL", "AC", "^", "/", 7, 8, 9, "*", 4, 5, 6, "-", 1, 2, 3, "+", "+/-", 0, ".", "="];
 
 symbols.map((element) => {
     const newBtn = document.createElement("button");
+
     newBtn.textContent = element;
 
     newBtn.classList.add('netBtn')
@@ -14,7 +15,7 @@ symbols.map((element) => {
     newBtn.addEventListener("click", function () {
         if (element === "AC") {
             result.textContent = "";
-        } else if (element === "x") {
+        } else if (element === "DEL") {
             const newString = result.textContent.slice(0, result.textContent.length - 1);
             result.textContent = newString;
         } else if (element === "=") {
@@ -22,11 +23,28 @@ symbols.map((element) => {
         } else {
             result.textContent = result.textContent + element;
         }
+
+
+
+
+        let i = 0
+        if (result.textContent[i] === "*") {
+            result.textContent = "";
+        } else if (result.textContent[i] === "/") {
+            result.textContent = "";
+        } else if (result.textContent[i] === "+") {
+            result.textContent = "";
+        } else if (result.textContent[i] === "-") {
+            result.textContent = "";
+        } else if (result.textContent[i] === "^") {
+            result.textContent = "";
+        }
+
     })
 })
 
 function calculate() {
-    const values = result.textContent.match(/(\d+\.?\d*|[\+\-\*\/])/g);
+    const values = result.textContent.match(/(\d+\.?\d*|[\+\^\-\*\/])/g);
 
     let results = 0;
 
@@ -41,11 +59,15 @@ function calculate() {
             values.splice(i - 1, 3, results);
             i--;
         } else if (values[i] === "+") {
-            results = values[i - 1] + values[i + 1];
+            results = Number(values[i - 1]) + Number(values[i + 1]);
             values.splice(i - 1, 3, results);
             i--;
         } else if (values[i] === "-") {
-            results = values[i - 1] - [i + 1];
+            results = values[i - 1] - values[i + 1];
+            values.splice(i - 1, 3, results);
+            i--;
+        } else if (values[i] === "^") {
+            results = values[i - 1] ** values[i + 1]
             values.splice(i - 1, 3, results);
             i--;
         } else {
@@ -54,6 +76,6 @@ function calculate() {
     }
 
 
-
     console.log(values);
+    result.textContent = values;
 }
